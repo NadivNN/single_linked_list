94726963103c9e76e30eec119a100d372466de1e
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-11-09 17:14:04 +0700

    Create method  list dan method addNode

--- single_linked_list/Program.cs
+++ single_linked_list/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace single_linked_list
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
+        public int noMhs;
+        public string nama;
+        public Node next;
+    }
+    class List
+    {
+        Node START;
+        public List()
         {
+            START = null;
         }
+
+        public void addNode()/*Method untuk menambahkan sebuah Node kedalam List*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs) /*Node ditambakan sebagai nodeBaru*/
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            /*Menemukan lokasi node baru di dalam list*/
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            /* Node baru akan ditetapkan diantara previous dan current*/
+
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
+
+
     }
 }
